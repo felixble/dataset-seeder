67ef0587d472ea50c86474ac35c844bded2a1c59
Felix Blechschmitt <felix@blechschmitt.name>
2018-08-24 14:37:28 +0200

    Delete affected tables if desired (default)
    Before inserting the rows into each table we first clear the table to get rid of outdated values. This behaviour can be changed by the additional flag ($clearTablesFirst)

--- src/DataSetSeeder.php
+++ src/DataSetSeeder.php
@@ -26,7 +26,7 @@ class DataSetSeeder extends Module {
      * @param $dataSetArray
      * @throws \Codeception\Exception\ModuleException
      */
-    public function seedDatabase($dataSetArray) {
+    public function seedDatabase($dataSetArray, $clearTablesFirst = true) {
         /** @var Db $dbModule */
         $dbModule = $this->getModule('Db');
 
@@ -34,6 +34,13 @@ class DataSetSeeder extends Module {
         $dbh = $dbModule->dbh;
 
         foreach ($dataSetArray as $table => $rows) {
+            if ($clearTablesFirst) {
+                $delQuery = "DELETE FROM $table";
+                $this->debugSection('Delete query', $delQuery);
+                $deleteSth = $dbh->query($delQuery);
+                $deleteSth->execute();
+            }
+            
             foreach ($rows as $row) {
                 $keys = [];
                 $values = [];
